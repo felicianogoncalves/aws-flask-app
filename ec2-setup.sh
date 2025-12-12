0ddcaaadad6bc374b9324fa7849c373eb6ecf676
FelicianoJúniorGonçalves <juniorsilvagoncalvez@gmail.com>
2025-12-12 12:42:35 +0000

    Ajustes no codigo ec2 com repositorio certo e ajuste da porta de saida no app.py

--- ec2-setup.sh
+++ ec2-setup.sh
@@ -6,13 +6,13 @@ apt update -y
 apt install -y python3-pip git
 
 # Clona o teu projeto do GitHub
-git clone https://github.com/braga10/meu-primeiro-flask.git /home/ubuntu/app
+git clone https://github.com/felicianogoncalves/aws-flask-app.git /home/ubuntu/app
 
 # Entra na pasta
 cd /home/ubuntu/app
 
 # Instala o Flask
-pip3 install flask
+pip3 install -r requirements.txt
 
 # Inicia a app em segundo plano na porta 80
 nohup python3 app.py > /home/ubuntu/app.log 2>&1 &
